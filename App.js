7da70c06a7127daad60a4cafcb32a306fdb4d147
krgitti <krgitti80@gmail.com>
2025-04-22 11:43:50 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import InicioScreen from './screens/InicioScreen';
+import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
 import NovoCadastroScreen from './screens/NovoCadastroScreen';
+import AtendimentoScreen from './screens/AtendimentosScreen';
+import DoacoesScreen from './screens/DoacoesScreen';
 import HistoricoScreen from './screens/HistoricoScreen';
-import LoginScreen from './screens/LoginScreen';
+import RelatoriosScreen from './screens/RelatoriosScreen';
+import ConfiguracoesScreen from './screens/ConfiguracoesScreen';
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="Inicio">
+        <Stack.Screen name="Inicio" component={InicioScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="NovoCadastro" component={NovoCadastroScreen} />
+        <Stack.Screen name="Atendimentos" component={AtendimentoScreen} />
+        <Stack.Screen name="Doacoes" component={DoacoesScreen} />
         <Stack.Screen name="Historico" component={HistoricoScreen} />
+        <Stack.Screen name="Relatorios" component={RelatoriosScreen} />
+        <Stack.Screen name="Configuracoes" component={ConfiguracoesScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
